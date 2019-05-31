c468cdeb4d16608e1565b9df67b7f4d0d7f02616
tanakaedu <tanaka@am1.jp>
2019-05-31 13:44:39 +0900

    完成

--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -12,9 +12,33 @@ namespace fukusyu0531
 {
 	public partial class Form1 : Form
 	{
+		int vx = -10;
+		int vy = -10;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			label1.Left += vx;
+			label1.Top += vy;
+
+			if (label1.Left < 0) {
+				vx = vx * -1;
+			}
+			if (label1.Top < 0)
+			{
+				vy = -vy;
+			}
+			if (label1.Left > 247) {
+				vx = -vx;
+			}
+			if (label1.Top > 247)
+			{
+				vy = -vy;
+			}
 		}
 	}
 }
